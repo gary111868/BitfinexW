a5e3b6329f060898135e5aa594cbcf1e16989e7a
Gary <62193371+gary111868@users.noreply.github.com>
2025-08-03 03:45:25 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/bitfinexws/main.go
@@ -0,0 +1,23 @@
+// cmd/bitfinexws/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"bitfinexws/internal/bitfinexws"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := bitfinexws.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
